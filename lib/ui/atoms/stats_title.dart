d84aabe6d64a9e290d71c79c8916bcb8c2037e6b
reitmas32 <rafa.zamora.rals@gmail.com>
2023-10-06 07:13:41 -0600

    feat: Create StatsTitle

---
+++ lib/ui/atoms/stats_title.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StatsTitle extends StatelessWidget {
+  const StatsTitle({
+    super.key,
+    required this.lable,
+    required this.percentage,
+    required this.color,
+    this.leadding,
+  });
+
+  final String lable;
+  final double percentage;
+  final Color color;
+  final Icon? leadding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            lable,
+            style: GoogleFonts.outfit(
+              fontSize: 12,
+              color: color,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Spacer(),
+          Text(
+            '$percentage%',
+            style: GoogleFonts.outfit(
+                fontSize: 12,
+                color: Theme.of(context).colorScheme.onPrimary,
+                fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}

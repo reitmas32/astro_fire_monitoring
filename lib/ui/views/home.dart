c3aba314a7e36bdfe95bcccc0e4152cb05322182
reitmas32 <rafa.zamora.rals@gmail.com>
2023-10-06 07:22:13 -0600

    feat: Create HomeView

---
+++ lib/ui/views/home.dart
@@ -0,0 +1,46 @@
+import 'package:astro_fire_monitoring/ui/providers/sidebar_controller.dart';
+import 'package:astro_fire_monitoring/ui/views/map_ui.dart';
+import 'package:astro_fire_monitoring/ui/views/notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeView extends ConsumerWidget {
+  const HomeView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
+    final controller = ref.watch(sideBarControllerProvider);
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        final pageTitle = _getTitleByIndex(controller.selectedIndex);
+        switch (controller.selectedIndex) {
+          case 0:
+            return NotificationFires();
+          case 1:
+            return MapUI();
+          default:
+            return Text(
+              pageTitle,
+              style: theme.textTheme.headlineSmall,
+            );
+        }
+      },
+    );
+  }
+}
+
+String _getTitleByIndex(int index) {
+  switch (index) {
+    case 2:
+      return 'Aqui va la vista de la camara del dron';
+    case 3:
+      return 'Settings';
+
+    default:
+      return 'Not found page';
+  }
+}

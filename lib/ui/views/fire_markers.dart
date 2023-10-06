66f586a620f98e962fefb656e4621a3fc726b9b8
reitmas32 <rafa.zamora.rals@gmail.com>
2023-10-06 07:47:39 -0600

    feat: Update Markers and use the providers by get the Data

--- lib/ui/views/fire_markers.dart
+++ lib/ui/views/fire_markers.dart
@@ -1,4 +1,5 @@
 import 'package:astro_fire_monitoring/ui/providers/map_controller.dart';
+import 'package:astro_fire_monitoring/ui/providers/marks.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,33 +13,24 @@ class FireMarkers extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final controller = ref.watch(mapControllerProvider);
+    final marks = ref.watch(getMarksFires);
     return MarkerLayer(
-      markers: [
-        Marker(
-          point: LatLng(19.462909481822955, -99.04031221700238),
-          width: 50,
-          height: 50,
-          builder: (context) => Image.network(
-              'https://cdn-icons-png.flaticon.com/512/599/599502.png'),
-        ),
-        Marker(
-          point: LatLng(20, -100),
-          width: 50,
-          height: 50,
-          builder: (context) => MouseRegion(
-            onHover: (event) => print('Hola con el zoom ${controller.zoom}'),
-            child: Image.network(
-                'https://cdn-icons-png.flaticon.com/512/599/599502.png'),
-          ),
-        ),
-        Marker(
-          point: LatLng(20, -101),
-          width: 50,
-          height: 50,
-          builder: (context) => Image.network(
-              'https://cdn-icons-png.flaticon.com/512/599/599502.png'),
-        ),
-      ],
+      markers: marks.when(
+          data: (data) => data.map(
+                (e) {
+                  print(LatLng(e.latitude, e.longitude).toString());
+                  return Marker(
+                    point: LatLng(e.latitude, e.longitude),
+                    width: 50,
+                    height: 50,
+                    builder: (context) => Image.network(
+                      'https://cdn-icons-png.flaticon.com/512/599/599502.png',
+                    ),
+                  );
+                },
+              ).toList(),
+          error: (_, __) => [],
+          loading: () => []),
     );
   }
 }

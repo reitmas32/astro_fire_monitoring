c5d85fe4a43b4e31f8ebfbf3381bd6f2516bfb5a
reitmas32 <rafa.zamora.rals@gmail.com>
2023-10-06 07:08:39 -0600

    feat: Create Principal Page

---
+++ lib/ui/astrofire_monitoring.dart
@@ -0,0 +1,27 @@
+import 'package:astro_fire_monitoring/ui/views/astrofire_sidebar.dart';
+import 'package:astro_fire_monitoring/ui/views/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AstroFireMonitoring extends ConsumerWidget {
+  AstroFireMonitoring({super.key});
+
+  final _key = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      key: _key,
+      body: Row(
+        children: [
+          AstroFireSideBar(),
+          Expanded(
+            child: Center(
+              child: HomeView(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
